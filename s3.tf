da670dca0a1a7b873ab1d2ed0df53252f438ddc1
bharathkumarmani <barat.2020@gmail.com>
2025-06-24 14:33:49 +0530

    added s3 bucket versioning resource

--- s3.tf
+++ s3.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "my-tea-bucket-tf"{
         Environment = "QA and Dev"  
       }
 }
+
+
+resource "aws_s3_bucket_versioning" "my-tea-bucket-tf-versioning" {
+    bucket = aws_s3_bucket.my-tea-bucket-tf.id
+    
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
